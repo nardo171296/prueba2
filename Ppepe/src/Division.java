4f61d9cac01557435dac90c5ec9bdcb7c7b8e201
bernardo <bernardo@bernardo-VirtualBox>
2018-03-21 16:17:01 +0100

    PEPE1-D

---
+++ Ppepe/src/Division.java
@@ -0,0 +1,24 @@
+/*
+*Plantilla de clases creada por el usuario $(user)
+* en la fecha $(date) y hora $(time)
+
+* Contactar en: bernardolrrr@gmail.com
+*/
+
+/**
+ *
+ * @author Bernardo 
+ */
+public class Division {
+   private int a;
+private int b;
+
+public Division(int a, int b) {
+ this.a = a;
+ this.b = b;
+ }
+ public int divide() {
+ return a/b;
+ }
+
+}
